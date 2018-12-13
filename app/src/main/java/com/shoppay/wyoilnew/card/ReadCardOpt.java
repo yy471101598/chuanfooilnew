d4fd70f403c6c9354025e5bac90fc61c5fc11404
wangyang2018 <471101598@qq.com>
2018-12-13 10:53:21 +0800

    1812131053

--- app/src/main/java/com/shoppay/wyoilnew/card/ReadCardOpt.java
+++ app/src/main/java/com/shoppay/wyoilnew/card/ReadCardOpt.java
@@ -16,8 +16,7 @@ import com.sunmi.payservice.hardware.aidl.bean.PayCardInfo;
  * sunmi P1N检卡读卡
  * 联系方式QQ：398320879
  */
-public class ReadCardOpt extends Activity
-{
+public class ReadCardOpt extends Activity {
 
     private static final String TAG = "ReadCardOpt";
 
@@ -27,8 +26,7 @@ public class ReadCardOpt extends Activity
     private EditText textView;  //接受需要显示卡号的控件
     private HardwareOpt mHardwareOpt; //sunmiP1n 硬件接口操作模块
 
-    public ReadCardOpt()
-    {
+    public ReadCardOpt() {
 
     }
 
@@ -37,24 +35,19 @@ public class ReadCardOpt extends Activity
      *
      * @param textView 显示卡号的控件
      */
-    public ReadCardOpt(EditText textView)
-    {
+    public ReadCardOpt(EditText textView) {
         this.textView = textView;
 
         mHardwareOpt = MyApplication.sHardwareOpt;
 
-        if (mHardwareOpt != null)
-        {
-            try
-            {
+        if (mHardwareOpt != null) {
+            try {
                 /**
                  * 1：Mag磁条卡   8：mifare感应卡
                  * 开始检卡
                  */
                 mHardwareOpt.checkCard(1 | 8, readCardCallback, time);
-            }
-            catch (RemoteException e)
-            {
+            } catch (RemoteException e) {
                 e.printStackTrace();
             }
         }
@@ -63,11 +56,9 @@ public class ReadCardOpt extends Activity
     /**
      * 检卡回调
      */
-    private ReadCardCallback readCardCallback = new ReadCardCallback.Stub()
-    {
+    private ReadCardCallback readCardCallback = new ReadCardCallback.Stub() {
         @Override
-        public void onCardDetected(PayCardInfo payCardInfo) throws RemoteException
-        {
+        public void onCardDetected(PayCardInfo payCardInfo) throws RemoteException {
             Log.d(TAG, "读取成功: ");
             //解析并读取卡号
             Message message = new Message();
@@ -76,54 +67,42 @@ public class ReadCardOpt extends Activity
             mHandler.sendMessage(message);
 
             //读卡成功后反复调用
-            try
-            {
+            try {
                 mHardwareOpt.checkCard(1 | 8, readCardCallback, time);
-            }
-            catch (RemoteException e)
-            {
+            } catch (RemoteException e) {
                 e.printStackTrace();
             }
         }
 
         @Override
-        public void onError(int i, String s) throws RemoteException
-        {
+        public void onError(int i, String s) throws RemoteException {
             if (!s.equals("重复调用")) Log.e(TAG, "onError:" + i + " " + s);
 
             //读卡失败或读卡超时重新调用检卡程序
-            try
-            {
+            try {
                 mHardwareOpt.checkCard(1 | 8, readCardCallback, time);
-            }
-            catch (RemoteException e)
-            {
+            } catch (RemoteException e) {
                 e.printStackTrace();
             }
         }
 
         @Override
-        public void onStartCheckCard() throws RemoteException
-        {
+        public void onStartCheckCard() throws RemoteException {
             Log.e(TAG, "onStartCheckCard:正在检卡");
         }
 
     };
 
 
-    private Handler mHandler = new Handler()
-    {
+    private Handler mHandler = new Handler() {
 
         @Override
-        public void handleMessage(Message msg)
-        {
+        public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            switch (msg.what)
-            {
+            switch (msg.what) {
                 case 0x01:
                     PayCardInfo payCardInfo = (PayCardInfo) msg.obj;
-                    if (payCardInfo.cardType == 8)
-                    {
+                    if (payCardInfo.cardType == 8) {
                         /***************Mifare 感应卡***********************/
                         String uuid = payCardInfo.uuid;
                         //取每两位十六进制逆向排列为市面上的通用卡号
@@ -131,7 +110,8 @@ public class ReadCardOpt extends Activity
                         //通用卡号
                         mCardNumber = String.valueOf(Long.parseLong(common_uuid, 16));
                         //非通用卡号
-                        mMifareCardNumber = String.valueOf(Long.parseLong(payCardInfo.uuid, 16));
+//                        mMifareCardNumber = String.valueOf(Long.parseLong(payCardInfo.uuid, 16));
+                        mMifareCardNumber = String.format("%010d", Long.parseLong(payCardInfo.uuid, 16));
 
                         /**
                          * 正式使用时，mCardNumber是市面上90%读卡器识别出来的卡号
@@ -139,13 +119,10 @@ public class ReadCardOpt extends Activity
                          * 默认显示通用卡号
                          */
                         textView.setText(mMifareCardNumber);
-                    }
-                    else
-                    {
+                    } else {
                         /***************Mag磁条卡*********************/
                         mCardNumber = payCardInfo.track2;
-
-                        textView.setText(mCardNumber);
+                        textView.setText(String.format("%010d", mCardNumber));
                     }
                     break;
             }
@@ -154,15 +131,11 @@ public class ReadCardOpt extends Activity
 
 
     @Override
-    public void onBackPressed()
-    {
-        try
-        {
+    public void onBackPressed() {
+        try {
             // 终止检卡
             overReadCard();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         super.onBackPressed();
@@ -173,14 +146,10 @@ public class ReadCardOpt extends Activity
      *
      * @throws RemoteException
      */
-    public void overReadCard() throws RemoteException
-    {
-        try
-        {
+    public void overReadCard() throws RemoteException {
+        try {
             MyApplication.sHardwareOpt.cancelCheckCard();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
